ba438702afde3318dd99702bbea152cf595a7d5f
Olga <113819035+Monacolga@users.noreply.github.com>
2023-12-21 17:47:03 +0100

    feat: add mineswipper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  const result = [];
+  let count;
+  for (let i = 0; i < rows; i += 1) {
+    result.push([]);
+    for (let k = 0; k < cols; k += 1) {
+      count = 0;
+
+      for (let x = -1; x <= 1; x += 1) {
+        for (let y = -1; y <= 1; y += 1) {
+          if (x !== 0 || y !== 0) {
+            const newX = i + x;
+            const newY = k + y;
+
+            if (
+              newX >= 0 &&
+              newX < rows &&
+              newY >= 0 &&
+              newY < cols &&
+              matrix[newX][newY]
+            ) {
+              count += 1;
+            }
+          }
+        }
+      }
+      result[i].push(count);
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
